c7228d06cbab73dc66f2b790f44be16a9803ca0d
Gianmarco Cotellessa <g.cotellessa@betacom.it>
2019-04-17 16:55:11 +0200

    Initial Commit

---
+++ AlamofireApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AlamofireApp
+//
+//  Created by Gianmarco Cotellessa on 17/04/2019.
+//  Copyright © 2019 Gianmarco Cotellessa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
